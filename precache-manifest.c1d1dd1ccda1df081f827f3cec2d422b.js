8a7ce0fe93db4beeacba645b789cf2782cbd17a8
devshaffan <devshaffan@gmail.com>
2021-10-05 06:39:23 -0700

    Updates

---
+++ precache-manifest.c1d1dd1ccda1df081f827f3cec2d422b.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a29f1c5574733c18d2c",
+    "url": "/demo_urdu_add/static/css/main.b70369e1.chunk.css"
+  },
+  {
+    "revision": "4a29f1c5574733c18d2c",
+    "url": "/demo_urdu_add/static/js/main.412381a6.chunk.js"
+  },
+  {
+    "revision": "0ef13e3c32d25ee777b7",
+    "url": "/demo_urdu_add/static/js/runtime~main.bb8ff005.js"
+  },
+  {
+    "revision": "f2728174042a1cd44f3e",
+    "url": "/demo_urdu_add/static/css/2.f6b3fd85.chunk.css"
+  },
+  {
+    "revision": "f2728174042a1cd44f3e",
+    "url": "/demo_urdu_add/static/js/2.0cb4d463.chunk.js"
+  },
+  {
+    "revision": "cb2803db2e4bd086feba",
+    "url": "/demo_urdu_add/static/js/3.efcad361.chunk.js"
+  },
+  {
+    "revision": "40d8e09c1bc1e01df2f2d0350718c089",
+    "url": "/demo_urdu_add/static/media/_1.40d8e09c.mp3"
+  },
+  {
+    "revision": "65a6b97dc7801719b2d140c796276265",
+    "url": "/demo_urdu_add/static/media/_2.65a6b97d.mp3"
+  },
+  {
+    "revision": "6bd419110b7fb9b6b44fd2f477a35abc",
+    "url": "/demo_urdu_add/static/media/_3.6bd41911.mp3"
+  },
+  {
+    "revision": "ae5c58d75da67259d8f8a689eb4040cd",
+    "url": "/demo_urdu_add/static/media/_4.ae5c58d7.mp3"
+  },
+  {
+    "revision": "fa32eb88ca708bfdd1f506d0d6ad7163",
+    "url": "/demo_urdu_add/static/media/_5.fa32eb88.mp3"
+  },
+  {
+    "revision": "639f363f9c8589d7472737d7d86ca22b",
+    "url": "/demo_urdu_add/static/media/_6.639f363f.mp3"
+  },
+  {
+    "revision": "7fc0455545ca15ac611aabfd2aa9d5fc",
+    "url": "/demo_urdu_add/static/media/_7.7fc04555.mp3"
+  },
+  {
+    "revision": "9c9b9bcb08b586e22914148c5f69bcd6",
+    "url": "/demo_urdu_add/static/media/_8.9c9b9bcb.mp3"
+  },
+  {
+    "revision": "fc30493cbaf5493f1204486e9e9cbbf1",
+    "url": "/demo_urdu_add/static/media/_9.fc30493c.mp3"
+  },
+  {
+    "revision": "594c0b0ed7da5faa738c12be102b3018",
+    "url": "/demo_urdu_add/static/media/_10.594c0b0e.mp3"
+  },
+  {
+    "revision": "91412d063f2fde22edf3a015fa63a786",
+    "url": "/demo_urdu_add/static/media/removeItem.91412d06.mp3"
+  },
+  {
+    "revision": "74562e5d9b36cc740a89848ba488bf7d",
+    "url": "/demo_urdu_add/static/media/rooster.74562e5d.png"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/demo_urdu_add/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "6d3e6e31968de6abd43c75375d97964a",
+    "url": "/demo_urdu_add/static/media/ca.6d3e6e31.mp3"
+  },
+  {
+    "revision": "cabc22c6e80bc043c10b098b6b8854bb",
+    "url": "/demo_urdu_add/static/media/bowl.cabc22c6.png"
+  },
+  {
+    "revision": "c040d63c530ff46d273f95822ecb12e7",
+    "url": "/demo_urdu_add/static/media/football10.c040d63c.png"
+  },
+  {
+    "revision": "c989b2046b4fa39573d918b056723605",
+    "url": "/demo_urdu_add/static/media/football1.c989b204.png"
+  },
+  {
+    "revision": "fe892d45a0838c6408beabefe99b4aa6",
+    "url": "/demo_urdu_add/static/media/bouncingFootball.fe892d45.wav"
+  },
+  {
+    "revision": "f5f258693cd37ccdf82ff06568ee9082",
+    "url": "/demo_urdu_add/static/media/background.f5f25869.gif"
+  },
+  {
+    "revision": "b512cd285a5ca16e6e3ec0c535047a76",
+    "url": "/demo_urdu_add/static/media/background2.b512cd28.gif"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/demo_urdu_add/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/demo_urdu_add/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "6e3309482bfd5d33e89a0368495a77de",
+    "url": "/demo_urdu_add/index.html"
+  }
+];
